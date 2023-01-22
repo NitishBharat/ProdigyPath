c59f58397796b5b8123a86d46efe61eb2c578b43
NitishBharat <97412786+NitishBharat@users.noreply.github.com>
2023-01-22 13:21:55 +0530

    Add files via upload

---
+++ assets/includes/head.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en" dir="ltr" class="overflow-hidden">
+
+<!-- #Head -->
+
+<?php include("assets/includes/seo.php"); ?>
+
+<head>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="author" content="<?php echo $lang_array['site-name']; ?>" />
+    <meta name="robots" content="all" />
+
+    <!-- Links -->
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Merriweather:ital@0;1&family=Oswald:wght@500&display=swap" />
+    <link rel="stylesheet" href="assets/plugins/bootstrap/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/plugins/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="assets/plugins/magnific-isotope/magnific-popup.min.css" />
+    <link rel="stylesheet" href="assets/plugins/sweetalert/sweetalert.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+    <!-- Links End -->
+
+
+
+    <!-- #Social Media -->
+
+    <link rel="icon" href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jpg" sizes="16x16" type="image/png" />
+    <link rel="icon" href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" sizes="32x32" type="image/png" />
+    <link rel="icon" href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" sizes="96x96" type="image/png" />
+
+    <link rel="apple-touch-icon" sizes="57x57"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="apple-touch-icon" sizes="72x72"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="apple-touch-icon" sizes="76x76"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="apple-touch-icon" sizes="114x114"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="apple-touch-icon" sizes="120x120"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="apple-touch-icon" sizes="144x144"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="apple-touch-icon" sizes="152x152"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="apple-touch-icon" sizes="180x180"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+
+    <link rel="icon" type="image/png" sizes="36x36"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="icon" type="image/png" sizes="48x48"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="icon" type="image/png" sizes="72x72"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="icon" type="image/png" sizes="96x96"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="icon" type="image/png" sizes="144x144"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+    <link rel="icon" type="image/png" sizes="192x192"
+        href="https://i.postimg.cc/bvr1ZF3c/prodgy-Path.jp" />
+
+    <link rel="alternate" hreflang="en" href="https://pack2net.com/" />
+    <link rel="alternate" hreflang="x-default" href="https://pack2net.com/" />
---
+++ assets/includes/header.php
@@ -0,0 +1,71 @@
+<header>
+
+    <div class="top-header">
+        <div class="container">
+            <div class="text-end">
+                <a href="tel:+201222898163"><svg class="svg phone" viewBox="0 0 512 512">
+                        <path
+                            d="M487.8 24.1L387 .8c-14.7-3.4-29.8 4.2-35.8 18.1l-46.5 108.5c-5.5 12.7-1.8 27.7 8.9 36.5l53.9 44.1c-34 69.2-90.3 125.6-159.6 159.6l-44.1-53.9c-8.8-10.7-23.8-14.4-36.5-8.9L18.9 351.3C5 357.3-2.6 372.3.8 387L24 487.7C27.3 502 39.9 512 54.5 512 306.7 512 512 307.8 512 54.5c0-14.6-10-27.2-24.2-30.4zM55.1 480l-23-99.6 107.4-46 59.5 72.8c103.6-48.6 159.7-104.9 208.1-208.1l-72.8-59.5 46-107.4 99.6 23C479.7 289.7 289.6 479.7 55.1 480z" />
+                    </svg>+20 122 2898 163</a>
+                <a href="mailto:info@pack2net.com" class="ms-4">info@pack2net.com</a>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+
+            <a class="navbar-brand" href="./">
+                <svg class="svg logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 640">
+                    <style>
+                    .logo-text {
+                        fill: #ffffff;
+                    }
+
+                    .logo-img {
+                        fill: #30a7ff;
+                    }
+                    </style>
+                    
+                </svg>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"><svg class="svg" viewBox="0 0 384.97 384.97">
+                        <path
+                            d="M12.03,120.303h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03 c-6.641,0-12.03,5.39-12.03,12.03C0,114.913,5.39,120.303,12.03,120.303z" />
+                        <path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03
+S379.58,180.455,372.939,180.455z" />
+                        <path d="M372.939,264.667H132.333c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h240.606
+c6.641,0,12.03-5.39,12.03-12.03C384.97,270.056,379.58,264.667,372.939,264.667z" />
+                    </svg></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto metismenu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services.php">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="portfolio.php">Resources</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="get-quote.php">Guidance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contacts.php">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+
+</header>
